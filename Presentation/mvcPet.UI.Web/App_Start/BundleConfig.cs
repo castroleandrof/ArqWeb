b1c3cdb2702e43d0969c1a19f047dd90d5175f9e
Aulas Unpaz <aiunpaz@outlook.com>
2019-08-26 19:46:17 -0300

    proyecto

--- Presentation/mvcPet.UI.Web/App_Start/BundleConfig.cs
+++ Presentation/mvcPet.UI.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace mvcPet.UI.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darkly.css",
                       "~/Content/site.css"));
         }
     }
