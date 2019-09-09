1c5d8fc80c1b98e9979862041da7e67d46464ea6
julian9510 <49660696+julian9510@users.noreply.github.com>
2019-09-09 02:00:19 -0300

    Add files via upload

---
+++ Business/mvcPet.Business/TipoServicioComponent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using mvcPet.Entities;
+using mvcPet.Data;
+
+namespace mvcPet.Business
+{
+    public partial class TipoServicioComponent
+    {
+        public TipoServicio Agregar(TipoServicio tipoServicio)
+        {
+            TipoServicio result = default(TipoServicio);
+            var tipoServicioDAC = new TipoServicioDAC();
+            result = tipoServicioDAC.Create(tipoServicio);
+            return result;
+        }
+        public List<TipoServicio> ListarTodos()
+        {
+            List<TipoServicio> result = default(List<TipoServicio>);
+            var tipoServicioDAC = new TipoServicioDAC();
+            result = tipoServicioDAC.Read();
+            return result;
+
+        }
+
+        public void Edit(TipoServicio especie)
+        {
+            var especieDAC = new TipoServicioDAC();
+            especieDAC.Update(especie);
+        }
+
+        public void Delete(int Id)
+        {
+            var especieDAC = new TipoServicioDAC();
+            especieDAC.Delete(Id);
+        }
+
+        public TipoServicio Details(int Id)
+        {
+            TipoServicio result = default(TipoServicio);
+            var tipoServicioDAC = new TipoServicioDAC();
+            result = tipoServicioDAC.ReadBy(Id);
+            return result;
+
+        }
+    }
+}
