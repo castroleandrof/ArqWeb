36a578946749fddc343137a3819ac06dd49d05c2
julian9510 <49660696+julian9510@users.noreply.github.com>
2019-09-09 02:09:31 -0300

    Add files via upload

--- Shared/mvcPet.Entities/TipoServicio.cs
+++ Shared/mvcPet.Entities/TipoServicio.cs
@@ -1,17 +1,16 @@
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 
 namespace mvcPet.Entities
 {
-    public partial class Especie : IEntity
+    public partial class TipoServicio : IEntity
     {
-        
         [DisplayName("Id")]
         public int Id { get; set; }
 
-        
-        [DisplayName("Nombre de la especie")]
+
+        [DisplayName("Nombre del Tipo de Servicio")]
         [Required]
         public string Nombre { get; set; }
     }
